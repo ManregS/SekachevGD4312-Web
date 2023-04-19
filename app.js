6114c44903b5cdd81da4f85db9f05e007c35fa57
german.sekachev <german.sekachev@innostage-group.ru>
2023-04-19 13:23:03 +0300

    Лабортаорная работа 5

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var main = function () {
     "use strict";
     var toDos = [
-        "Закончить писать эту книгу",
-        "Вывести Грейси на прогулку в парк",
-        "Ответить на электронные письма",
-        "Подготовиться к лекции в понедельник",
-        "Обновить несколько новых задач",
-        "Купить продукты"
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Duis et ligula laoreet tortor pharetra accumsan.",
+        "Sed blandit libero a dictum imperdiet.",
+        "Etiam eget ligula sed libero dapibus consectetur."
     ];
     $(".tabs a span").toArray().forEach(function (element) {
         $(element).on("click", function () {
@@ -15,7 +13,11 @@ var main = function () {
             $element.addClass("active");
             $(".content").empty();
             if ($element.parent().is(":nth-child(1)")) {
-                console.log("Щелчок на первой вкладке!");
+                $content = $("<ul>");
+				for (var i = toDos.length - 1; i > -1; i--) {
+					$content.append($("<li>").text(toDos[i]));
+				}
+				$(".content").append($content);
             } else if ($element.parent().is(":nth-child(2)")) {
                 $content = $("<ul>");
                 toDos.forEach(function (todo) {
@@ -23,11 +25,26 @@ var main = function () {
                 });
                 $(".content").append($content);
             } else if ($element.parent().is(":nth-child(3)")) {
-                console.log("Щелчок на третьей вкладке!");
+                $(".content").append("<p>");
+                $(".content").append("<input>");
+                $(".content").append("<br>");
+                $(".content").append("<p>");
+				$(".content").append("<button>Добавить</button>");
+				$(".content input").addClass("input");
+				$(".content button").addClass("button");
             }
             return false;
         })
     });
+    $(".content").on("click", ".button", function() {
+		if ($(".input").val() != "") {
+			toDos.push($(".input").val());
+			alert("Предложение успешно добавлено в список!");
+		}
+		else {
+			alert("ERROR: Длина добавляемого предложения должна быть > 0");
+		}
+	});
 };
 
 main();
